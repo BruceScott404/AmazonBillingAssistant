36fc61e3dade466af1f9a485b1d6242c7b1f7ea7
Bruce Scott <bscott97@uwo.ca>
2024-06-21 09:55:06 -0400

    Added installer, and helper script

---
+++ install.bat
@@ -0,0 +1,8 @@
+@echo off
+color 0a
+title "Installer"
+
+del input\.gitkeep
+del output\.gitkeep
+
+pip3 -u install pandas
---
+++ run.bat
@@ -0,0 +1,7 @@
+@echo off
+title Amazon Billing Assistant
+
+python3 assistant.py
+
+echo "Service Complete..."
+pause
